5503caea85b03842a68546c913892951068a460e
247joseph <247josephjose@gmail.com>
2026-07-04 23:25:48 +0530

    refined

--- _nuxt/logo.f4a21025.js
+++
@@ -1,5 +0,0 @@
-import "./entry.9eba175a.js";
-const _imports_0 = "" + globalThis.__publicAssetsURL("images/logo.png");
-export {
-  _imports_0 as _
-};
--- components/Welcome/_payload.js
+++
@@ -1,1 +0,0 @@
-export default {data:{},prerenderedAt:1783093741411}
